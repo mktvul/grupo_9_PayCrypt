0215001996bb29d20a838ce9fdab2157039dbf5f
mktvul <mkt.vul@gmail.com>
2021-10-30 17:55:33 -0300

    incorporando CRUD de usuarios

--- routers/mainRouters.js
+++ routers/mainRouters.js
@@ -1,13 +1,31 @@
-const express = require ('express');
+//Require's
+const express = require("express");
 const router = express.Router();
-const mainControllers = require ('../controllers/mainControllers');
 
-router.get ('/', mainControllers.index);
-router.get ('/details', mainControllers.details);
-router.get ('/login', mainControllers.login);
-router.get ('/register', mainControllers.register);
-router.get ('/cart', mainControllers.cart);
-router.get('/create', mainControllers.create);
-router.get('/edit', mainControllers.edit)
+//Controller
+const mainControllers = require("../controllers/mainControllers");
+
+//Index
+router.get("/", mainControllers.index);
+
+//Products
+router.get("/details", mainControllers.details);
+router.get("/cart", mainControllers.cart);
+router.get("/create", mainControllers.create);
+router.get("/edit", mainControllers.edit);
+
+//Users
+router.get("/login", mainControllers.login);
+
+//User - register
+router.get("/register", mainControllers.register);
+router.post("/", mainControllers.storeUser);
+
+//User - edit
+router.get("/edit/:id", mainControllers.editUser);
+router.put("/edit/:id", mainControllers.updateUser);
+
+//User - delete
+router.delete("/delete/:id", mainControllers.removeUser);
 
 module.exports = router;
