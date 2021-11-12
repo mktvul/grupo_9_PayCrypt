b8a259e0a6b09c00b23fbdd80513b93bf4dbacd9
mkt.vul <mkt.vul@gmail.com>
2021-11-12 19:41:22 -0300

    middleware de verificación de usuario logueado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Require's
 const express = require("express");
+const session = require("express-session");
 const path = require("path"); //Unifica rutas dentro de los sistemas operativos
 const methodOverride = require("method-override");
 
@@ -7,10 +8,22 @@ const methodOverride = require("method-override");
 const app = express();
 
 //Middlewares
+const userLogged = require("./middlewares/userLogged");
+
 app.use(express.static(path.join(__dirname, "./public"))); // Le digo a express que quiero tener la carpeta "public" como un recurso de archivos estáticos
 app.use(express.urlencoded({ extended: false })); //Convierte a json la info que viene por HTML desde el formulario
 app.use(express.json()); //Convierte a json la info que viene desde js cliente
 app.use(methodOverride("_method")); //Nos permite usar los métodos PUT y DELETE
+
+app.use(
+  session({
+    secret: "Shhh, It's a secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(userLogged); //Verifica que el usuario esté logueado
 
 //Template engine
 app.set("view engine", "ejs"); // Utiliza el motor de plantillas ejs
---
+++ middlewares/userLogged.js
@@ -0,0 +1,12 @@
+function userLogged(req, res, next) {
+  res.locals.isLogged = false;
+
+  if (req.session.userLogged) {
+    res.locals.isLogged = true;
+    res.locals.userLogged = req.session.userLogged;
+  }
+
+  next();
+}
+
+module.exports = userLogged;
