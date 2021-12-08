947361c1b35ba64307b60bc0fce2a58e7abdd0de
marquitos <marcoszulian600@gmail.com>
2021-12-08 12:24:03 -0300

    sin clave en config

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "test123",
+    "password": null,
     "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql"
