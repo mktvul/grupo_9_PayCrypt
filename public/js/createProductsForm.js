899914a3df76f1220d0816117afd8c25e06b7500
Maria Belen Legresti <mariabelenlegresti@gmail.com>
2021-12-28 19:36:37 -0300

    rehacemos la base de datos

---
+++ public/js/createProductsForm.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    let form = document.querySelector('#form');
+
+    form.addEventListener('submit', function(event) {
+        let errors = [];
+        
+    })
+});
