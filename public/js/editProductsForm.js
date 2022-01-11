3beb0dcfd471beb87aa73a175bf0d51de0c704c4
Maria Belen Legresti <mariabelenlegresti@gmail.com>
2022-01-10 22:16:46 -0300

    arreglo validaciones del front en productos

--- public/js/editProductsForm.js
+++ public/js/editProductsForm.js
@@ -12,9 +12,9 @@ window.addEventListener('load', function() {
         let category = document.querySelector('input[name = "categoryEdit"]:checked');
         let coin = document.querySelector('input[name = "coinEdit"]:checked');
 
-        if (name.value == '') {
+        if (name.value.length < 5) {
             name.style.border = "solid 2px #db3236";
-            errors.name = 'Debes completar este campo';
+            errors.name = 'Debes completar este campo con al menos 5 caracteres';
         }
 
         if (shortDescription.value == '') {
@@ -27,13 +27,18 @@ window.addEventListener('load', function() {
             errors.price = 'Debes completar este campo';
         }
 
-        if (largeDescription.value == '') {
+        if (largeDescription.value.length < 20) {
             largeDescription.style.border = "solid 2px #db3236";
-            errors.largeDescription = 'Debes completar este campo';
+            errors.largeDescription = 'Debes completar este campo con al menos 20 caracteres';
         }
 
-        if (photoProduct.value == '') {
-            errors.photoProduct = 'Debes subir una foto del producto';
+        let validExtension = [".jpg", ".jpeg", ".png", ".gif"];
+        let fileName = photoProduct.value.lastIndexOf(".");
+        let fileExtension = photoProduct.value.substring(fileName);
+        let isValid = validExtension.includes(fileExtension);
+
+        if (photoProduct.value == '' || !isValid) {
+            errors.photoProduct = 'Debes subir una foto del producto valida';
         }
 
         if (category == null) {
@@ -51,31 +56,31 @@ window.addEventListener('load', function() {
         if (Object.keys(errors).length >= 1) {
             event.preventDefault();
             if (errors.name) {
-                document.querySelector('.nameEdit').innerText = 'Debes completar este campo';
+                document.querySelector('.nameEdit').innerText = errors.name;
             }
             
             if (errors.shortDescription) {
-                document.querySelector('.shortDescriptionEdit').innerText = 'Debes completar este campo';
+                document.querySelector('.shortDescriptionEdit').innerText = errors.shortDescription;
             }
             
             if (errors.price) {
-                document.querySelector('.priceEdit').innerText = 'Debes completar este campo';
+                document.querySelector('.priceEdit').innerText = errors.price;
             }
             
             if (errors.largeDescription) {
-                document.querySelector('.largeDescriptionEdit').innerText = 'Debes completar este campo';
+                document.querySelector('.largeDescriptionEdit').innerText = errors.largeDescription;
             }
             
             if (errors.photoProduct) {
-                document.querySelector('.photoProductEdit').innerText = 'Debes subir una foto del producto';
+                document.querySelector('.photoProductEdit').innerText = errors.photoProduct;
             }
             
             if (errors.category) {
-                document.querySelector('.categoryEdit').innerText = 'Debes seleccionar una categoria';
+                document.querySelector('.categoryEdit').innerText = errors.category;
             }
             
             if (errors.coin) {
-                document.querySelector('.coinEdit').innerText = 'Debes seleccionar una moneda';
+                document.querySelector('.coinEdit').innerText = errors.coin;
             }
         }
     })
