ad0e18350ddfa830c038c9abdda1af4458253631
marquitos <marcoszulian600@gmail.com>
2022-01-21 11:17:04 -0300

    actualizando api

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,13 +1,109 @@
 const db = require("../database/models");
 
 
+ var deportes =0;
+    db.Product.findAll()
+      .then((product) => {
+        for (let i = 0; i < product.length; i++) {
+          if (product[i].categoryId == 1) {
+           deportes++;
+          }
+        }
+       
+})
+
+
+var inmuebles =0;
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].categoryId == 2) {
+        inmuebles++;
+      }
+    }
+   
+})
+
+
+
+var mineria =0;
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].categoryId == 3) {
+        mineria++;
+      }
+    }
+   
+})
+
+
+
+
+var nft =0;
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].categoryId == 4) {
+        nft++;
+      }
+    }
+   
+})
+
+
+
+
+
+var tecnologia =0;
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].categoryId == 5) {
+        tecnologia++;
+      }
+    }
+   
+})
+
+var vehiculos =0;
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].categoryId == 6) {
+        vehiculos++;
+      }
+    }
+   
+})
+
+
+
+var data =[];
+db.Product.findAll()
+  .then((product) => {
+    for (let i = 0; i < product.length; i++) {
+        data.push({id:product[i].id, name:product[i].name, description:product[i].description, detail:"http://localhost:3000/products/detail/"+ product[i].id })
+      }
+    
+})
+   
+
+
+
 module.exports =  {
     list: (req, res) =>{
         db.Product.findAll().then(
             Product=>{
                 return res.json({
                     total: Product.length,
-                    data: Product,
+                    totalDeDeportes:deportes,
+                    totalDeInmuebles: inmuebles,
+                    totalDeMineria: mineria,
+                    totalDeNft: nft,
+                    totalDeTecnologia: tecnologia,
+                    totalDeVehiculos: vehiculos,      
+                    data: data,
                     status:200
                 })
             }
@@ -15,11 +111,19 @@ module.exports =  {
     },
 
     productId: (req, res) =>{
-        db.Product.findByPk(req.params.id)
+        db.Product.findByPk(req.params.id, {
+            include: [
+              { association: "users" },
+              { association: "categories" },
+              { association: "coins" },
+              { association: "carts" },
+            ],
+          })
         .then(
             Product=>{
                 return res.json({
                     data: Product,
+                    categoria: Product.categories.name,
                     status:200
                 })
             }
