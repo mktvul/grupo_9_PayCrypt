68511e2ef983ad6effe131639dbf377582c1ef24
EspilTomas <tomasynaza@gmail.com>
2022-02-05 18:30:23 -0300

    fix api again x2

--- controllers/apiUserController.js
+++ controllers/apiUserController.js
@@ -8,7 +8,7 @@ let data = [];
 db.User.findAll()
     .then( users => {
         for (let i = 0; i < users.length; i++) {
-            data.push({id:users[i].id, name:users[i].name, email:users[i].email, detail:'http://localhost:3001/user/profile/'+ users[i].id, image: '/images/users/'+ users[i].image });
+            data.push({id:users[i].id, name:users[i].name, email:users[i].email, detail:'http://localhost:3001/user/profile/'+ users[i].id, image: 'http://localhost:3001/images/users/'+ users[i].image });
             
         }
     })
